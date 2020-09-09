c409d565d11f134b5c975212baf2e2c3dc30d333
iChoob <zdude1011@gmail.com>
2020-09-09 19:27:36 -0400

    Created Plugin

---
+++ src/main/java/htsearth/craftingrecipes/CraftingRecipes.java
@@ -0,0 +1,17 @@
+package htsearth.craftingrecipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftingRecipes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
