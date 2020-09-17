e26290418710be4fa00a0c163bab6b33d3f5da26
Olle <olle@Abergr-Macbook-Pro-2.local>
2020-09-17 16:16:05 +0200

    [+] Added Crying Obsidian Recipe #2, [Changed Netherite recipe to discussion in issue #1,

--- src/main/java/htsearth/craftingrecipes/CraftingRecipes.java
+++ src/main/java/htsearth/craftingrecipes/CraftingRecipes.java
@@ -9,25 +9,39 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CraftingRecipes extends JavaPlugin {
 
+	NamespacedKey cryingObsidianKey = new NamespacedKey(this, "crying_obsidian");
+	NamespacedKey netheriteKey = new NamespacedKey(this, "netherite_ingot");
+
     @Override
     public void onEnable() {
         // Plugin startup logic
     	ItemStack netherite = new ItemStack(Material.NETHERITE_INGOT);
     	
-    	NamespacedKey netheriteKey = new NamespacedKey(this, "netherite_ingot");
     	
     	ShapedRecipe netheriteRecipe = new ShapedRecipe(netheriteKey, netherite);
-    	netheriteRecipe.shape("DOD", "DED", "DOD");
+    	netheriteRecipe.shape("DOD", "DGD", "DOD");
     	netheriteRecipe.setIngredient('D', Material.DIAMOND_BLOCK);
     	netheriteRecipe.setIngredient('O', Material.OBSIDIAN);
-    	netheriteRecipe.setIngredient('E', Material.EMERALD_BLOCK);
+    	netheriteRecipe.setIngredient('G', Material.GOLD_BLOCK);
     	
     	Bukkit.addRecipe(netheriteRecipe);
+    	
+    	ItemStack cryingObsidian = new ItemStack(Material.CRYING_OBSIDIAN);
+    	
+    	
+    	ShapedRecipe cryingObsidianRecipe = new ShapedRecipe(cryingObsidianKey, cryingObsidian);
+    	cryingObsidianRecipe.shape("OLO");
+    	cryingObsidianRecipe.setIngredient('O', Material.OBSIDIAN);
+    	cryingObsidianRecipe.setIngredient('L', Material.LAPIS_LAZULI);
 
+    	Bukkit.addRecipe(cryingObsidianRecipe);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    	Bukkit.removeRecipe(cryingObsidianKey);
+    	Bukkit.removeRecipe(netheriteKey);
+
     }
 }
